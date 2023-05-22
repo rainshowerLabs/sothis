6b3ceb5c4973a909997b644cd272fa5693e3b659
Vukasin Gostovic <vukasin@gostovic.me>
2023-05-22 15:11:17 +0200

    cooking

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use reqwest::Client;
+use async_trait::async_trait;
 
 #[derive(Debug, Serialize)]
 struct JsonRpcRequest {
@@ -11,42 +12,63 @@ struct JsonRpcRequest {
 }
 
 #[derive(Debug, Deserialize)]
-#[warn(dead_code)]
+#[allow(dead_code)]
 struct JsonRpcResponse {
     jsonrpc: String,
     result: Value,
     id: u32,
 }
 
-pub async fn post(url: String, method: String, param: String) -> Result<String, Box<dyn std::error::Error>> {
-    let client = Client::new();
-
-    let request = JsonRpcRequest {
-        jsonrpc: "2.0".to_string(),
-        method,
-        params: json!(param),
-        id: 1,
-    };
-    
-    let response = client
-        .post(&url)
-        .json(&request)
-        .send()
-        .await?
-        .json::<JsonRpcResponse>()
-        .await?;
-    
-    let block_number_hex = response.result;
-    
-    Ok(block_number_hex.to_string())
+struct RpcConnection {
+    client: Client,
+    url: String
 }
 
-// Gets blocknumber
-pub async fn get_block_number(historical_rpc: String) -> Result<String, Box<dyn std::error::Error>> {
-    post(historical_rpc, "eth_blockNumber".to_string(), "".to_string()).await
+#[async_trait]
+trait RpcRequests {
+    fn new(url: String) -> Self;
+    async fn post(&self, url: String, method: String, param: String) -> Result<String, Box<dyn std::error::Error>>;
+    async fn block_number(&self) -> Result<String, Box<dyn std::error::Error>>;
+    async fn get_block_by_number(&self, block_number: String) -> Result<String, Box<dyn std::error::Error>>;
 }
 
-// Get block by number
-pub async fn get_block_by_number(historical_rpc: String, block_number: String) -> Result<String, Box<dyn std::error::Error>> {
-    post(historical_rpc, "eth_getBlockByNumber".to_string(), format!("\"{}\", true", block_number)).await
+#[async_trait]
+impl RpcRequests for RpcConnection {
+    fn new(url: String) -> Self {
+        Self {
+            client: Client::new(),
+            url: url
+        }
+    }
+
+    async fn post(&self, url: String, method: String, param: String) -> Result<String, Box<dyn std::error::Error>> {
+        let request = JsonRpcRequest {
+            jsonrpc: "2.0".to_string(),
+            method,
+            params: json!(param),
+            id: 1,
+        };
+        
+        let response = self.client
+            .post(&url)
+            .json(&request)
+            .send()
+            .await?
+            .json::<JsonRpcResponse>()
+            .await?;
+        
+        let block_number_hex = response.result;
+        
+        Ok(block_number_hex.to_string())
+    }
+
+    // Gets blocknumber
+    async fn block_number(&self) -> Result<String, Box<dyn std::error::Error>> {
+        self.post(self.url.clone(), "eth_blockNumber".to_string(), "".to_string()).await
+    }
+
+    // Get block by number
+    async fn get_block_by_number(&self, block_number: String) -> Result<String, Box<dyn std::error::Error>> {
+        self.post(self.url.clone(), "eth_getBlockByNumber".to_string(), format!("\"{}\", true", block_number)).await
+    }
 }
