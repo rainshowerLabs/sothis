f35eaac2042cbaf8f9879fb03cbd1fe508741bbd
Vukasin Gostovic <vukasin@gostovic.me>
2023-05-31 20:02:06 +0200

    CLAP

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,46 @@
 mod rpc;
 mod replay;
 
-use clap::{arg, Command};
+use clap::{Command, Arg};
 
 use crate::rpc::format::hex_to_decimal;
 use crate::rpc::format::format_number_input;
 use rpc::rpc::RpcConnection;
 use crate::replay::*;
 
-#[allow(unused_variables)]
+/*
+ * Settings
+ */
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = Command::new("sothis")
         .version("0.1.2")
         .author("makemake <vukasin@gostovic.me>")
-        .about("Tool for replaying historical transactions. Designed to be used with anvil/hh node")
-        .arg(arg!(--source_rpc <VALUE>).required(true).help("HTTP JSON-RPC of the nde we're querrying data from"))
-        .arg(arg!(--terminal_block <VALUE>).required(true).help("Block we're replaying until"))
-        .arg(arg!(--replay_rpc <VALUE>).required(true).help("HTTP JSON-RPC of the node we're replaying data to"))
+        .about("Tool for replaying historical transactions. Designed to be used with anvil")
+        .arg(Arg::new("source_rpc")
+            .long("source_rpc")
+            .short('s')
+            .num_args(1..)
+            .required(true)
+            .help("HTTP JSON-RPC of the node we're querying data from"))
+        .arg(Arg::new("terminal_block")
+            .long("terminal_block")
+            .short('b')
+            .num_args(1..)
+            .required(true)
+            .help("Block we're replaying until"))
+        .arg(Arg::new("replay_rpc")
+            .long("replay_rpc")
+            .short('r')
+            .num_args(1..)
+            .required(true)
+            .help("HTTP JSON-RPC of the node we're replaying data to"))
+        .arg(Arg::new("exit_on_tx_fail")
+            .long("exit_on_tx_fail")
+            .short('e')
+            .help("Exit the program if a transaction fails"))
         .get_matches();
 
     let source_rpc: String = matches.get_one::<String>("source_rpc").expect("required").to_string();
