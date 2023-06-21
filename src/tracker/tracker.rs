8f16c1db414b43905ace3af159b9504d231a88f9
Vukasin Gostovic <vukasin@gostovic.me>
2023-06-21 19:44:29 +0200

    serialize on sigterm/sigkill

--- src/tracker/tracker.rs
+++ src/tracker/tracker.rs
@@ -3,19 +3,29 @@ use crate::APP_CONFIG;
 use crate::RpcConnection;
 use crate::tracker::types::*;
 
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use ctrlc;
 use ethers::types::U256;
 
 // We listen for new blocks and get the storage slot value if changed.
-#[allow(unreachable_code)]
 pub async fn track_state(
     source_rpc: RpcConnection,
     storage_slot: U256,
     contract_address: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let interrupted = Arc::new(AtomicBool::new(false));
+    let interrupted_clone = interrupted.clone();
+
+    ctrlc::set_handler(move || {
+        interrupted_clone.store(true, Ordering::SeqCst);
+    })?;
+
 	let mut storage = StateChangeList {
 		storage_slot: storage_slot,
 		state_changes: Vec::new(),
 	};
+
 	// Release the lock immediately since we're gonna use it later
     let block_time;
     {
@@ -24,6 +34,10 @@ pub async fn track_state(
     }
 
 	loop {
+        if interrupted.load(Ordering::SeqCst) {
+            break;
+        }
+
 		let block_number = source_rpc.listen_for_blocks(block_time).await?;
 		let block_number = hex_to_decimal(&block_number)?; // FIXME: this returns a u64, change this
 		let latest_slot = source_rpc.get_storage_at(contract_address.clone(), storage_slot.clone()).await?;
@@ -38,6 +52,8 @@ pub async fn track_state(
 			storage.state_changes.push(slot);
 		}
 	}
+	let json = serde_json::to_string(&storage)?;
+	println!("Serialized storage: {}", json);
 
 	Ok(())
 }
