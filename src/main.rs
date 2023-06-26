3951ad06df4fa0ab76fd3db1822233388a3fa718
Vukasin Gostovic <vukasin@gostovic.me>
2023-06-26 14:54:38 +0200

    Bump version

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ lazy_static! {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = Command::new("sothis")
-        .version("0.3.0")
+        .version("0.3.1")
         .author("makemake <vukasin@gostovic.me>")
         .about("Tool for replaying historical transactions. Designed to be used with anvil or hardhat.")
         .arg(Arg::new("source_rpc")
