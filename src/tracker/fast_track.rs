49d21fc58a4a9f0c6c2293f7375889622513b813
Vukasin Gostovic <vukasin@gostovic.me>
2023-07-10 23:04:27 +0200

    add fast track untested

---
+++ src/tracker/fast_track.rs
@@ -0,0 +1,72 @@
+use crate::RpcConnection;
+use crate::rpc::format::{hex_to_decimal, decimal_to_hex};
+use crate::tracker::types::*;
+use crate::tracker::time::get_latest_unix_timestamp;
+
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::fs;
+
+use ctrlc;
+use ethers::types::U256;
+
+// We querry historical storage from a node instead of waiting for new blocks.
+pub async fn fast_track_state(
+    source_rpc: RpcConnection,
+    storage_slot: U256,
+    contract_address: String,
+    terminal_block: Option<u64>,
+    mut origin_block: u64,
+    path: String,
+    filename: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let interrupted = Arc::new(AtomicBool::new(false));
+    let interrupted_clone = interrupted.clone();
+
+    ctrlc::set_handler(move || {
+        interrupted_clone.store(true, Ordering::SeqCst);
+    })?;
+
+	let mut storage = StateChangeList {
+		address: contract_address.clone(),
+		storage_slot: storage_slot,
+		state_changes: Vec::new(),
+	};
+
+
+
+	while origin_block < terminal_block.unwrap_or(hex_to_decimal(&source_rpc.block_number().await?)?) {
+        if interrupted.load(Ordering::SeqCst) {
+            break;
+        }
+		
+		let latest_slot = source_rpc.get_storage_at_block(contract_address.clone(), storage_slot.clone(), decimal_to_hex(origin_block)).await?;
+		let slot = StateChange {
+			block_number: origin_block.into(),
+			value: latest_slot,
+		};
+
+		if storage.state_changes.last().map(|change| change.value != slot.value).unwrap_or(true) {
+			println!("New storage slot value: {:?}", &slot.value);
+			storage.state_changes.push(slot);
+		}
+
+		origin_block += 1;
+	}
+	
+	let json = serde_json::to_string(&storage)?;
+
+	// Set the filename to `address{contract_address}-slot-{storage_slot}-timestamp-{unix_timestamp} if its the default one
+	let filename = if filename == "" {
+		let timestamp = get_latest_unix_timestamp();
+		format!("address-{}-slot-{}-timestamp-{}.json", contract_address, storage_slot, timestamp)
+	} else {
+		filename
+	};
+
+	let path = format!("{}/{}", path, filename);
+	println!("\nWriting to file: {}", path);
+	fs::write(path, json)?;
+
+	Ok(())
+}
--- src/tracker/mod.rs
+++ src/tracker/mod.rs
@@ -1,3 +1,4 @@
 pub mod tracker;
 pub mod types;
 pub mod time;
+pub mod fast_track;
