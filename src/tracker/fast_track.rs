2f5b1debf8987494b8f706c2b745fbbe0839ac26
Vukasin Gostovic <vukasin@gostovic.me>
2023-07-25 03:13:02 +0200

    optimizoor

--- src/tracker/fast_track.rs
+++ src/tracker/fast_track.rs
@@ -80,24 +80,23 @@ pub async fn fast_track_state(
 	
 	// Set the filename to `address{contract_address}-slot-{storage_slot}-timestamp-{unix_timestamp} if its the default one
 	// We also check if we should serialize it as csv
-	let mut is_csv = false;
+	let json;
 	let filename = match filename.as_str() {
 		"" => {
 			let timestamp = get_latest_unix_timestamp();
 			println!("No filename specified, using default and formatting as JSON");
+			json = storage.serialize_json()?;
 			format!("address-{}-slot-{}-timestamp-{}.json", contract_address, storage_slot, timestamp)
 		},
 		filename if filename.contains(".csv") => {
 			println!("Formatting as CSV");
-			is_csv = true;
+			json = storage.serialize_csv();
 			filename.to_string()
 		},
-		_ => filename,
-	};
-
-	let json = match is_csv {
-		true => storage.serialize_csv(),
-		false => storage.serialize_json()?,
+		_ => {
+			json = storage.serialize_json()?;
+			filename
+		},
 	};
 
 	let path = format!("{}/{}", path, filename);
